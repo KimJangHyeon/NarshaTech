fd21381b8f2978ff32b6b673c68ae68c603ba188
KimJangHyeon <rlawkdgus75@gmail.com>
2017-07-20 11:08:45 +0900

    hompage, hosting4,5

--- front-end/scripts/slideshow.js
+++ front-end/scripts/slideshow.js
@@ -31,69 +31,50 @@
 //   dots[slideIndex-1].className += " active";
 // }
 
-(function() {
+// (
+var slideIndex = 1;
 
-	function Slideshow( element ) {
-		this.el = document.querySelector( element );
-		this.init();
-	}
-
-	Slideshow.prototype = {
-		init: function() {
-			this.wrapper = this.el.querySelector( ".slider-wrapper" );
-			this.slides = this.el.querySelectorAll( ".slide" );
-			this.previous = this.el.querySelector( ".slider-previous" );
-			this.next = this.el.querySelector( ".slider-next" );
-			this.index = 0;
-			this.total = this.slides.length;
-			this.timer = null;
-
-			this.action();
-			this.stopStart();
-		},
-		_slideTo: function( slide ) {
-			var currentSlide = this.slides[slide];
-			currentSlide.style.opacity = 1;
-
-			for( var i = 0; i < this.slides.length; i++ ) {
-				var slide = this.slides[i];
-				if( slide !== currentSlide ) {
-					slide.style.opacity = 0;
-				}
-			}
-		},
-		action: function() {
-			var self = this;
-			self.timer = setInterval(function() {
-				self.index++;
-				if( self.index == self.slides.length ) {
-					self.index = 0;
-				}
-				self._slideTo( self.index );
-
-			}, 9000);
-		},
-		stopStart: function() {
-			var self = this;
-			self.el.addEventListener( "mouseover", function() {
-				clearInterval( self.timer );
-				self.timer = null;
-
-			}, false);
-			self.el.addEventListener( "mouseout", function() {
-				self.action();
-
-			}, false);
-		}
+var test = document.getElementById('dot');
+test.addEventListener()
 
 
-	};
+showSlides(slideIndex);
 
-	document.addEventListener( "DOMContentLoaded", function() {
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-		var slider = new Slideshow( "#main-slider" );
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-	});
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
+var slideIndex = 0;
+showSlides();
 
-})();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 500); // Change image every 2 seconds
+}
