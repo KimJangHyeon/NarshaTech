226dbe1df57502ac3bee50d59513d47066321ce3
KimJangHyeon <rlawkdgus75@gmail.com>
2017-07-20 15:24:53 +0900

    hosting design not

--- front-end/scripts/upload.js
+++ front-end/scripts/upload.js
@@ -1,9 +1,17 @@
 /**
  * Created by rlawk on 2017-07-20.
  */
+var index=0;
 function addPicture() {
-    var x = document.createElement("INPUT");
-    x.setAttribute("type", "file");
-    document.getElementById("img_block").appendChild(x);
+    if(index!=10){
+        var x = document.createElement("INPUT");
+        x.setAttribute("type", "file");
+        document.getElementById("img_block").appendChild(x);
+        index++;
+    }
+    if(index==10){
+        var x= document.getElementBy
+        document.getElementById("img_block").removeChild();
+    }
     // document.body.appendChild(x);
 }
