9c2210590059bd79b95500fdaf2fb5deb2533b46
= <=>
2017-07-26 09:42:01 +0900

    change name activity->signup

--- back-end/CampusTown/hosting/models.py
+++ back-end/CampusTown/hosting/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from login import UserForm
 
 # Create your models here.
+class Host(models.Model) :
+    id = Model.ForeignField(UserForm)
+    picture = Model.CharField(max_length = 200, )
+    university = Model.CharField(max_length = 50)
+    location = Model.CharField(max_length = 175)
+    introduction = Model.CharField(max_length = 500)
+    universityIntroduction = Model.CharField(max_length = 500)
+    totalRating = Model.FloatField()
--- back-end/CampusTown/mysite/settings.py
+++ back-end/CampusTown/mysite/settings.py
@@ -45,9 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'activity',
+    'signup',
 	'login',
 	'main',
+	'activity',
 ]
 
 MIDDLEWARE = [
