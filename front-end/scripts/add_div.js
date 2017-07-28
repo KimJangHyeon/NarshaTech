37aaa127daa2791e3d7bc4e260df0d9b37673f16
KimJangHyeon <rlawkdgus75@gmail.com>
2017-07-28 10:34:55 +0900

    del marking, connect marking with intro, mult-marking

--- front-end/scripts/add_div.js
+++ front-end/scripts/add_div.js
@@ -1,6 +1,8 @@
 /**
  * Created by rlawk on 2017-07-27.
  */
+//http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+
 function add_div(){
     var div = document.createElement('div');
     div.innerHTML = document.getElementById('intro_block').innerHTML;
