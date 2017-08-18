6b4d36601e144f5973756ca672c31001c0cc6e20
= <=>
2017-08-19 00:27:09 +0900

    modify form

--- back-end/CampusTown/activity_detail/forms.py
+++ back-end/CampusTown/activity_detail/forms.py
@@ -1,32 +1,38 @@
 from django import forms
 from .models import ActivityInfo, ActivitySchedule, ActivityPicture, HashTag, ActivityReview, Host #, ActivityLanguage
+from django.contrib.auth.models import User
 
 class ActivityDetail(forms.ModelForm) :
     class Meta :
         model = ActivityInfo
-        fields = []
+        fields = ["title", "category", "gatheringityLocation", "activityIntroduction", "vehicle", "meetingPlace", "price", "maximumCapacity", "additionalInformation", "readyTime", "lastReadyTime", "activityRating", "totalTime"]
 
 class ActivityDetailPicture(forms.ModelForm) :
     class Meta :
         model = ActivityPicture
-        fields = []
+        fields = ["url"]
 
 class ActivityDetailSchedule(forms.ModelForm) :
     class Meta :
         model = ActivitySchedule
-        fields = []
+        fields = ["location", "url", "introduction"]
 
 class ActivityDetailHashTag(forms.ModelForm) :
     class Meta :
         model = HashTag
-        fields = []
+        fields = ["url"]
 
 class ActivityDetailReview(forms.ModelForm) :
     class Meta :
         model = ActivityReview
-        fields = []
+        fields = ["user_id", "review", "date", "rating"]
 
 class HostForm(forms.ModelForm) :
     class Meta :
         model = Host
-        fields = []
+        fields = ["picture", "university", "location", "introduction", "universityIntroduction", "totalRating"]
+
+class UserForm(forms.ModelForm) :
+    class Meta :
+        model = User
+        fields = ["last_name", "first_name", "gender"]
