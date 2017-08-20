d69443408ead81b85bd6a3cb740d216d0d390a65
= <=>
2017-08-20 17:09:04 +0900

    register activity related models for managing in admin site

--- back-end/CampusTown/activity/admin.py
+++ back-end/CampusTown/activity/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Activity, ActivitySchedule, ActivityPicture, HashTag, ActivityReview, BookMark
 
 # Register your models here.
+admin.site.register(Activity)
+admin.site.register(ActivitySchedule)
+admin.site.register(ActivityPicture)
+admin.site.register(HashTag)
+admin.site.register(ActivityReview)
